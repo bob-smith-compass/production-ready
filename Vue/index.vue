30f42ba0a42c251048439fe16d10e68bcacfe5f4
David Shams <david.shams@gmail.com>
2019-02-07 01:52:30 -0600

    Vue is great!

---
+++ Vue/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Welcome to {message}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Vue'
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    background-color: yellowgreen;
+    color: red;
+}
+</style>
+
