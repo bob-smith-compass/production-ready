59247f25ba99e31f1e97eae7755434e3240f81b0
David Shams <david.shams@gmail.com>
2019-02-09 20:21:25 -0600

    Semantic UI components

--- Vue/index.vue
+++ Vue/index.vue
@@ -1,5 +1,12 @@
 <template>
+<div>
+
     <h1>Welcome to {message}</h1>
+
+    <input class="button newsletter-button" type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
+    <input class="button newsletter-button" type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
+
+</div>
 </template>
 
 <script>
@@ -16,6 +23,24 @@ export default {
 h1 {
     background-color: yellowgreen;
     color: red;
+}
+
+#news .newsletter-input {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px 80px 10px 20px;
+  height: 50px;
+  border-radius: 50px;
+  border: 1px solid #ccc;
+  font-size: 16px;
+}
+#news .newsletter-button.button {
+  position: absolute;
+  padding: 4px 20px;
+  margin: 0;
+  height: calc(100% - 8px);
+  right: 4px;
+  top: 4px;
 }
 </style>
 
