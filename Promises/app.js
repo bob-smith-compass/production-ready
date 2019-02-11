d5b5369a06ab3277b086620ff69827665a1edaf0
David Shams <david.shams@gmail.com>
2019-02-11 03:06:49 -0600

    click 5 times to win Promise

--- Promises/app.js
+++ Promises/app.js
@@ -1,20 +1,35 @@
 
-function startGate() {
+function startGame() {
     let counter = 0;
     document.querySelectorAll('#promise')[0].addEventListener('click', () => {
 
         ++counter;
         console.log(`Clicked ${counter} times`);
 
-    })
-    setTimeout(() => {
-        if (counter > 5) {
-            console.log('You won!');
-        } else {
-            console.log('You lost!')
-        }
-        console.log('2 seconds passed!');
-    }, 2000)
+    });
+    /**
+     * 
+     setTimeout(() => {
+         if (counter > 5) {
+             console.log('You won!');
+            } else {
+                console.log('You lost!')
+            }
+            console.log('2 seconds passed!');
+        }, 2000)
+        */
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            if (counter > 5) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 2000);
+    });
 }
 
-startGate();
+startGame()
+.then(()=> alert('You win!'))
+.catch(() => alert('You lost!'));
