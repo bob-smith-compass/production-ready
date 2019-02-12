f71b774aad521669f4fac6929fd332c416f4482e
David Shams <david.shams@gmail.com>
2019-02-12 06:01:32 -0600

    ALWAYS HAVE [0] querySelectorAll('body')[0]

--- Cross-browser/app.js
+++ Cross-browser/app.js
@@ -1,8 +1,17 @@
 window.onload = start;
 function start() {
     console.log('DOM Loaded');
+    /**
+     * Not working
+     */
+    // document.querySelectorAll('body')[0].addEventListener('click', function () {
+    //     console.log('Mlicked!');
+    // });
 }
 
 // document.querySelectorAll('body').addEventListener('click', function() {
 //     console.log('Clicked!');
 // });
+document.querySelectorAll('body')[0].addEventListener('click', function () {
+    console.log('Mlicked!');
+});
