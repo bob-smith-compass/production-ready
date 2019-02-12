cd8f41824385391662f648fc35b7ec7a4d9b935c
David Shams <david.shams@gmail.com>
2019-02-12 05:57:07 -0600

    NOT WORKING: document.querySelectorAll('body').addEventListener('click', function() {

--- Cross-browser/app.js
+++ Cross-browser/app.js
@@ -2,3 +2,7 @@ window.onload = start;
 function start() {
     console.log('DOM Loaded');
 }
+
+document.querySelectorAll('body').addEventListener('click', function() {
+    console.log('Clicked!');
+});
