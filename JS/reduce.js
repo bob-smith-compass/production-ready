59bb4f28873fc6c954d5bdd8a6112be0e7c5f1d3
David Shams <david.shams@gmail.com>
2019-02-21 16:33:58 -0600

    reduce improved

--- JS/reduce.js
+++ JS/reduce.js
@@ -3,7 +3,9 @@ class Utility {
         this.numbers = [1,2,3,4,5,6]
     }
     sum() {
-        console.log(this.numbers.reduce((init, next) => init + next))
+        let total = this.numbers.reduce((init, next) => init + next);
+        console.log(`Total: ${total}`);
+        return total;
     }
 }
 console.log(new Utility());
